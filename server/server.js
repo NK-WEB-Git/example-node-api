465dfb6db8866e83743cdb02071bedd6e1a607fa
Boris Stephane NKUINJEU <nkuinjeustephane@gmail.com>
2017-07-23 23:29:58 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { mongoose } =  require('./db/mongoose');
+const { ObjectID } = require('mongodb');
 const { Todo } =  require('./models/todo');
 const { User } =  require('./models/user');
 
@@ -26,6 +27,21 @@ app.get('/todos', (req, res) => {
   }, (err) => {
     res.status(400).send(err);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send({message: 'Todo not found'});
+    }
+    res.send({ todo });
+  }).catch(err => res.status(400).send());
 });
 
 app.listen(3000, () => {
