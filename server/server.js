16bef9acd9177f0d4a2a2d1d4138cadca5726b18
Boris Stephane NKUINJEU <nkuinjeustephane@gmail.com>
2017-07-30 23:27:33 +0200

    Add winston logger

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const winston = require('winston');
 const bodyParser = require('body-parser');
-const { mongoose } =  require('./db/mongoose');
 const { ObjectID } = require('mongodb');
+
+const { mongoose } =  require('./db/mongoose');
 const { Todo } =  require('./models/todo');
 const { User } =  require('./models/user');
+
+const fs = require( 'fs' );
+const path = require('path');
+const logDir = `${__dirname}/../var/logs`; // directory path you want to set
+
+if ( !fs.existsSync(logDir)) {
+  // Create the directory if it does not exist
+  fs.mkdirSync(logDir);
+}
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +22,31 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+/**
+ * @api {post} /todos Register a new Todo
+ * @apiGroup Todos
+ * @apiParam {String} text Todo text
+ * @apiParamExample {json} Input
+ *    {
+ *      "text": "Study"
+ *    }
+ * @apiSuccess {Number} _id Todo unique _id
+ * @apiSuccess {Number} __v Todo __v
+ * @apiSuccess {String} text Todo text
+ * @apiSuccess {Boolean} completed Todo is completed or not ?
+ * @apiSuccess {Date} completedAt Todo's date completed
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "__v": 0,
+ *      "text": "Study",
+ *      "_id": "597dc57a859fe900110d5f34",
+ *      "completedAt": null,
+ *      "completed": false
+ *    }
+ * @apiErrorExample {json} Error unknown
+ *    HTTP/1.1 400 Bad Request
+ */
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text
@@ -27,24 +63,24 @@ app.post('/todos', (req, res) => {
  * @api {get} /todos List all todos
  * @apiGroup Todos
  * @apiSuccess {Object[]} todos Todo's list
- * @apiSuccess {Number} todos.id Todo id
- * @apiSuccess {Number} todos.__V Todo __v
+ * @apiSuccess {Number} todos._id Todo id
+ * @apiSuccess {Number} todos.__v Todo __v
  * @apiSuccess {String} todos.text Todo text
- * @apiSuccess {Boolean} todos.completed Todo is completed ?
+ * @apiSuccess {Boolean} todos.completed Todo is completed or not ?
  * @apiSuccess {Date} todos.completedAt Todo's date completed
  * @apiSuccessExample {json} Success
  *    HTTP/1.1 200 OK
  *    {
  *      todos:[
  *        {
- *          "id": 1,
+ *          "_id": 1,
  *          "text": "Study",
  *          "completed": false
  *          "completedAt": null,
  *          "__v": 0
  *        },
  *        {
- *          "id": 2,
+ *          "_id": 2,
  *          "text": "Sleep",
  *          "completed": true
  *          "completedAt": 2017-12-04,
@@ -53,7 +89,7 @@ app.post('/todos', (req, res) => {
  *      ]
  *    }
  * @apiErrorExample {json} List error
- *    HTTP/1.1 400 Internal Server Error
+ *    HTTP/1.1 400 Bad Request
  */
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
@@ -63,6 +99,32 @@ app.get('/todos', (req, res) => {
   });
 });
 
+/**
+ * @api {get} /todos/:id Find a todo
+ * @apiGroup Todos
+ * @apiParam {Number} _id Todo unique id
+ * @apiSuccess {Number} _id Todo unique _id
+ * @apiSuccess {Number} __v Todo __v
+ * @apiSuccess {String} text Todo text
+ * @apiSuccess {Boolean} completed Todo is completed or not ?
+ * @apiSuccess {Date} completedAt Todo's date completed
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "__v": 0,
+ *      "text": "Buy Macbook",
+ *      "_id": "597dc57a859fe900110d5f34",
+ *      "completedAt": null,
+ *      "completed": false
+ *    }
+ * @apiErrorExample {json} Error unknown
+ *    HTTP/1.1 400 Bad Request
+ * @apiErrorExample {json} Todo not found
+ *    HTTP/1.1 404 Not Found
+ *    {
+ *      "message": "Todo not found"
+ *    }
+ */
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id;
 
@@ -81,5 +143,30 @@ app.get('/todos/:id', (req, res) => {
 app.listen(port, () => {
   console.log(`Started at the port ${port}`);
 });
+
+const logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.File)({
+      name: 'info-file',
+      filename: path.join(logDir, 'info.log'),
+      level: 'info',
+      prettyPrint: true,
+    }),
+    new (winston.transports.File)({
+      name: 'error-file',
+      filename: path.join(logDir, 'error.log'),
+      handleExceptions: true,
+      level: 'error',
+      prettyPrint: true,
+    }),
+    new (winston.transports.Console)({
+      level: 'debug',
+      colorize: true
+    }),
+  ]
+});
+
+logger.debug('debug test');
+logger.error('error test');
 
 module.exports = { app };
