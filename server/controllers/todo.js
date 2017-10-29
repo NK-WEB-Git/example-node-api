f9840350bf971abfb66a95ea66051f0468f45302
Boris Stephane NKUINJEU <nkuinjeustephane@gmail.com>
2017-10-29 22:50:17 +0100

    Add token when user is created

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -1,8 +1,30 @@
-class TodoController {
-  constructor(req, res) {
-    
-  }
-  addTodo() {
+const { Todo } =  require('./../models/todo');
 
+class TodoController {
+
+  constructor(param) {
+    this.text = param.text || null;
+  }
+
+  addTodo() {
+    const todo = new Todo({
+      text: this.text,
+    });
+
+    todo.save().then((doc) => {
+      this.response.send(doc);
+    }, (err) => {
+      this.response.status(400).send(err);
+    });
+  }
+
+  getTodo() {
+    Todo.find().then((todos) => {
+      this.response.send({todos});
+    }, (err) => {
+      this.response.status(400).send(err);
+    });
   }
 }
+
+module.exports = { TodoController };
--- server/routers/user.js
+++ server/routers/user.js
@@ -33,14 +33,14 @@ const { User } =  require('./../models/user');
  */
 router
   .post('', (req, res) => {
-
     const body =_.pick(req.body, ['email', 'password']);
-
     const user = new User(body);
 
-    user.save().then((doc) => {
-      res.send(doc);
-    }, (err) => {
+    user.save().then(() => {
+      return user.generateAuthToken();
+    }).then((token) => {
+      res.header('x~auth', token).send(user);
+    }).catch((err) => {
       res.status(400).send(err);
     });
 });
